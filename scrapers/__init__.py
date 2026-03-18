6678c8cdda8272b24b92f3a21c64d67f204dcd28
fdrop123 <fdrop123@gmail.com>
2026-03-18 21:14:57 +0000

    Delete scrapers directory

--- scrapers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .autotrader import scrape_autotrader
-from .ebay import scrape_ebay
-from .gumtree import scrape_gumtree
-from .facebook import scrape_facebook
-
-__all__ = ["scrape_autotrader", "scrape_ebay", "scrape_gumtree", "scrape_facebook"]
